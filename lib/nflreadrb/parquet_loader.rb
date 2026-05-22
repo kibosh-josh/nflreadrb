c7eefe8e83f80c71df127d13b77ac6da8840afba
Joshua Pearson <joshua.david.pearson@gmail.com>
2026-05-22 09:59:38 -0600

    Refactor / Structurual change: Update ParquetLoader to be more similar to other classes and use a class method that calls .new and an instance method

--- lib/nflreadrb/parquet_loader.rb
+++ lib/nflreadrb/parquet_loader.rb
@@ -3,35 +3,50 @@
 require_relative 'local_cache'
 
 module Nflreadrb
-  module ParquetLoader
-    class << self
-      FILTER_COL = 'season'
+  class ParquetLoader
+    SEASON_FILTER = 'season'
 
-      def fetch_and_filter(url:, year:, columns: nil)
-        # 1. Delegate the file retrieval entirely to the cache manager
-        local_path = LocalCache.path_for(url)
-
-        # 2. Execute the Polars data transformation operations natively
-        requested_columns = columns&.map(&:to_s)
-        dataframe = Polars.read_parquet(local_path, columns: download_columns_from(requested_columns))
-
-        # 3. Filter vertically by season
-        filtered_dataframe = dataframe.filter(Polars.col(FILTER_COL) == year.to_i)
-
-        # 4. Filter horizontally to isolate user-requested columns if present
-        filtered_dataframe = filtered_dataframe.select(requested_columns) if requested_columns
-
-        filtered_dataframe.to_a
-      end
-
-      private
-
-      def download_columns_from(requested_columns)
-        return nil if requested_columns.nil?
-
-        ([FILTER_COL] + requested_columns).uniq
-      end
+    def self.fetch_and_filter(year:, url:, columns: nil)
+      new(year:, url:, columns:).fetch_and_filter
     end
+
+    def initialize(year:, url:, columns:)
+      @year = year.to_i
+      @url = url
+      @columns = columns&.map(&:to_s)
+    end
+    private_class_method :new
+
+    def fetch_and_filter
+      # 1. Delegate the file retrieval entirely to the cache manager
+      local_path = LocalCache.path_for(url)
+
+      # 2. Execute the Polars data transformation operations natively
+      dataframe = Polars.read_parquet(local_path, columns: download_columns_from(columns))
+
+      # 3. Filter results
+      filtered_dataframe_from(dataframe, columns)
+    end
+
+    private
+
+    def download_columns_from(columns)
+      return nil if columns.nil?
+
+      ([SEASON_FILTER] + columns).uniq
+    end
+
+    def filtered_dataframe_from(dataframe, columns)
+      # 3a. Filter vertically by season
+      filtered_dataframe = dataframe.filter(Polars.col(SEASON_FILTER) == year)
+
+      return filtered_dataframe.to_a if columns.nil?
+
+      # 3b. Filter horizontally to isolate user-requested columns if present
+      filtered_dataframe.select(columns).to_a
+    end
+
+    attr_reader :year, :url, :columns
   end
 
   private_constant :ParquetLoader
